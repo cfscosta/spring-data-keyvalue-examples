db76cdaa45c9f8e7fd3fd072fd4d3d9bd6a552c3
cfscosta <carlos.fsilvacosta@gmail.com>
2015-08-28 15:49:50 +0100

    retwisj now correctly displays all the messages

--- retwisj/src/main/java/org/springframework/data/redis/samples/retwisj/RetwisSecurity.java
+++ retwisj/src/main/java/org/springframework/data/redis/samples/retwisj/RetwisSecurity.java
@@ -25,40 +25,43 @@ import org.springframework.util.StringUtils;
  */
 public abstract class RetwisSecurity {
 
-	private static class UserInfo {
-		String name;
-		String uid;
-	}
+    private static class UserInfo {
+        String name;
+        String uid;
+    }
 
-	private static final ThreadLocal<UserInfo> user = new NamedThreadLocal<UserInfo>("Retwis-id");
+    private static final ThreadLocal<UserInfo> user = new NamedThreadLocal<UserInfo>("Retwis-id");
 
-	public static String getName() {
-		UserInfo userInfo = user.get();
-		return (userInfo != null ? userInfo.name : null);
-	}
+    public static String getName() {
+        UserInfo userInfo = user.get();
+        return (userInfo != null ? userInfo.name : null);
+    }
 
-	public static String getUid() {
-		UserInfo userInfo = user.get();
-		return (userInfo != null ? userInfo.uid : null);
-	}
+    public static String getUid() {
+        UserInfo userInfo = user.get();
+        return (userInfo != null ? userInfo.uid : null);
+    }
 
-	public static void setUser(String name, String uid) {
-		UserInfo userInfo = new UserInfo();
-		userInfo.name = name;
-		userInfo.uid = uid;
-		user.set(userInfo);
-	}
+    public static void setUser(String name, String uid) {
+        UserInfo userInfo = new UserInfo();
+        userInfo.name = name;
+        userInfo.uid = uid;
+        user.set(userInfo);
+    }
 
-	public static boolean isUserSignedIn(String name) {
-		UserInfo userInfo = user.get();
-		return userInfo != null && userInfo.name.equals(name);
-	}
+    public static boolean isUserSignedIn(String uid) {
+        UserInfo userInfo = user.get();
+        if (userInfo == null || userInfo.uid == null) {
+            return false;
+        }
+        return userInfo.uid.equals(uid);
+    }
 
-	public static boolean isSignedIn() {
-		return StringUtils.hasText(getName());
-	}
+    public static boolean isSignedIn() {
+        return StringUtils.hasText(getName());
+    }
 
-	public static void clean() {
-		user.set(null);
-	}
+    public static void clean() {
+        user.set(null);
+    }
 }
